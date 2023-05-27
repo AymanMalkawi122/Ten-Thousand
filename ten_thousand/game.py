b494fb5c66086c0dff21b82d45d4ac1a5d70bf81
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-05-27 13:59:08 +0300

    temp

---
+++ ten_thousand/game.py
@@ -0,0 +1,20 @@
+
+from game_logic import GameLogic
+
+class Game:
+    def __init__(self):
+        self.logic = GameLogic()
+        self.logic.reset()
+
+    def play(self, max_round = 1):
+        while self.logic.total_score < 10000 and self.logic.game_state != "exit_game" and self.logic.current_round <= max_round:
+            self.logic.game_iteration()
+        
+        if self.logic.game_state != "exit_game":
+            self.logic.game_state = "quit_game"
+            self.logic.game_iteration()
+
+
+if __name__ == "__main__":
+       game = Game()
+       game.play(1)
